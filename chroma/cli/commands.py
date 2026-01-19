968d169d74a0815155f9c4572cb1d3a9f1fb09ee
AbstractGleidson <gleidsonabstract.evgm@gmail.com>
2026-01-18 22:36:50 -0300

    First version of the functional image and webcam commands

--- chroma/cli/commands.py
+++ chroma/cli/commands.py
@@ -9,13 +9,18 @@ def chroma():
 @click.command()
 @click.option("--image", default=(PATH_IMAGES / "2.jpeg"), help="Caminho da imagem que deseja aplicar o efeito.")
 @click.option("--color", default=None, help="Cor do plano de fudo da imagem. No caso default a cor é verde.")
-@click.option("--back", default=(PATH_IMAGES / "4.jpg"), help="Imagem que deseja aplicar de plano de fundo. No caso defaul a cor é preta")
+@click.option("--back", default=(PATH_IMAGES / "4.jpg"), help="Imagem que deseja aplicar de plano de fundo. No caso default a cor é preta.")
 def image(image, back, color):
    SegImage(image, back, color)
 
 @click.command()
-def webcam():
-    SegWebCam()
+@click.option("--webcam", default=0, help="Index da webcam. Por padrão é 0.")
+@click.option("--back", default=None, help="Imagem que deseja aplicar de plano de fundo. No caso default a cor é preta.")
+@click.option("--color", default=None, help="Cor do plano de fundo da Webcam. No caso default a cor é verde.")
+@click.option("--h", default=640, help="Altura da janela. Por padrão 640px.")
+@click.option("--w", default=480, help="Largura da janela. Por padrao 480px")
+def webcam(webcam, back, color, h, w):
+    SegWebCam(webcam, back, color, h, w)
 
 chroma.add_command(image)
 chroma.add_command(webcam)
--- chroma/cli/core.py
+++ chroma/cli/core.py
@@ -6,6 +6,7 @@ import cv2 as openCV
 
 def SegImage(image, background, color):
     
+    # tenta ler imagem e plano de fundo
     image = openCV.imread(image)
     background = openCV.imread(background)
     
@@ -15,6 +16,7 @@ def SegImage(image, background, color):
     else:
         height, width = image.shape[:2]
         
+        # Configura plano de fundo
         if background is None:
             background = numpy.full((height, width, 3), (0, 0, 0)).astype("uint8")
         else:
@@ -24,14 +26,20 @@ def SegImage(image, background, color):
                 interpolation=openCV.INTER_CUBIC
             )
         
-        if color in ["green", "red", "blue"]:
-            color = readColorYaml(color)
-
+        # Leitura da cor 
+        if color is not None and color.lower() in ["green", "red", "blue"]:
+            color = readColorYaml(color.lower())
         else:
             color = readColorYaml("green")
-        
+            
+        mask = Segmentation.chromaKey(
+            image, 
+            background, 
+            color['min'], color['max']
+        )
         
         openCV.imshow('Imagem', image)
+        openCV.imshow('Imagem chroma', mask)
         openCV.imshow('Plano de fundo', background) # type: ignore
         
         key = openCV.waitKey(0) & 0xFF
@@ -39,5 +47,56 @@ def SegImage(image, background, color):
         if key == ord('q'):
             openCV.destroyAllWindows()
 
-def SegWebCam():
-    click.echo("Segmentacao de webcam")
+def SegWebCam(webcam, background, color, height, width):
+    
+    webcam = openCV.VideoCapture(webcam)
+    
+    if webcam is None:
+        click.echo("Não foi possível abrir a webcam.")
+        
+    else:
+        
+        # Configura tamanho da janela
+        webcam.set(openCV.CAP_PROP_FRAME_HEIGHT, height)
+        webcam.set(openCV.CAP_PROP_FRAME_WIDTH, width)
+        
+        background = openCV.imread(background)
+        
+        if background is None:
+            background = numpy.full((width, height, 3), (0, 0, 0)).astype("uint8")
+        else:
+            background = openCV.resize(
+                background,
+                (width, height),
+                interpolation=openCV.INTER_CUBIC
+            )
+        
+        if color is not None and color.lower() in ["green", "red", "blue"]:
+            color = readColorYaml(color.lower())
+        else:
+            color = readColorYaml("green")   
+            
+        while(True):
+            
+            isAvaliable, frame = webcam.read()
+            
+            if isAvaliable:
+                
+                mask = Segmentation.chromaKey(
+                    frame, 
+                    background, 
+                    color['min'], 
+                    color['max']
+                )
+                
+                openCV.imshow("Frame", frame)
+                openCV.imshow("Chroma", mask)
+                openCV.imshow("Plano de fundo", background)
+                
+                key = openCV.waitKey(1) & 0xFF
+                
+                if key == ord('q'):
+                    break
+        
+        webcam.release()
+        openCV.destroyAllWindows()
